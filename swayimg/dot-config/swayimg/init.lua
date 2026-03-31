897dd49e8c5d1c06cba034a13bd3273c06004c65
Jay-716 <13422525511@163.com>
2026-03-31 20:14:55 +0800

    swayimg: adapt to 5.0 lua config

---
+++ swayimg/dot-config/swayimg/init.lua
@@ -0,0 +1,8 @@
+swayimg.toggle_fullscreen()
+
+swayimg.viewer.on_key("q", swayimg.exit)
+swayimg.viewer.set_window_background(0xAA000000)
+
+swayimg.text.set_font("Source Han Sans CN")
+
+swayimg.text.set_timeout(1)
